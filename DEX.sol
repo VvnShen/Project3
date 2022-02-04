dae8f72fc4887f092e1aab379033ecf83083d59a
Vivian Shen <88476898+VvnShen@users.noreply.github.com>
2022-02-03 22:19:34 -0500

    Update DEX.sol

--- DEX.sol
+++ DEX.sol
@@ -1,29 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.4;
-import "github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 import "github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol";
 
-contract UsdcToken is ERC20 {
-    address payable owner;
-
-    modifier onlyOwner {
-        require(msg.sender == owner, "You do not have permission to mint these tokens!");
-        _;
-    }
-    
-    constructor(address payable settler) ERC20("USDCtest", "USDC") public {
-        owner = payable(settler);
-        _mint(owner, 100);
-    }
-//ERC20 in latest version doesn't support decimals specification, default=18. USDC is 6 decimal units. So I'll overload it.
-        function decimals() public view virtual override returns (uint8) {
-        return 6;
-    }
-
-    function mint(address recipient, uint amount) public onlyOwner {
-        _mint(recipient, amount);
-    }
-}
 
  /** Dex_ETH_owner_trading: initializer owns ETH, and want to swap with USDC. 
     1) required fileds to be input by initializer (Gather in front end): 
@@ -58,19 +36,22 @@ contract Dex_ETH_owner_trading {
         require (msg.sender == initializer, "ETH owner must be the initializer here");
         _;
     }
+
+
     event Initialize();
     event SettleSwap();
 
-    constructor (address payable settler) payable  public{
-        initializer = payable(msg.sender);
-        init_amount=msg.value;
-        token = new UsdcToken(settler);
+ 
+    constructor () payable {
+        initializer= payable(msg.sender);
+        init_amount= msg.value;
+
     }
 
-//initializer who owns ETH, input the ETH value as the amount to swap USDC
+//this contract will be initialized bt whom owns Ether and wants to swap to UDDC
     function initialize() 
         external 
-        inState(State.Created) condition(msg.value != 0)
+        inState(State.Created) condition(init_amount == msg.value)
         payable {
         emit Initialize();
         initializer=payable(msg.sender);
@@ -78,23 +59,30 @@ contract Dex_ETH_owner_trading {
 
     }
 
-    function check_address () view public returns(address) {
-        return address(this);
-    }
-
 //once a settler (who would call Dex_USDC_owner_trading) choose to settle this swap, call below function to settle for ETH owner
-//amount of USDC to swap will be calculated in FE, here just pass an amount of USDC for settlement (FE: USDC_amount=price*init_amont_in_ETH)
-    function settleSwap (address payable settler, uint USDC_amount)
+//amount of USDC to swap will be calculated in FE, here just pass an amount of USDC for settlement (FE: USDC_amount=price*init_amont_in_ETH*1000000)
+
+
+
+    function settleSwap (address payable settler,address _token, uint USDC_amount)
         external
-        inState(State.Locked) OnlyOwner condition(msg.value==init_amount)
+        inState(State.Locked) OnlyOwner condition(init_amount != 0)  
         payable {
             emit SettleSwap();
             state = State.Release;
-            uint256 allowance = token.allowance(settler, address(this));
-            require(allowance >= USDC_amount, "Check the token allowance");
-            settler.transfer(msg.value);
-            token.transferFrom(settler,initializer, USDC_amount);
+            token = IERC20(_token);
+            require (token.allowance(settler,address(this)) >= USDC_amount,"please make sure settler approve the token's delegation");
+            settler.transfer(init_amount);
+            _safeTransferFrom(token,settler,initializer, USDC_amount);
 
         }
 
+    function _safeTransferFrom(
+        IERC20 _token,
+        address sender,
+        address recipient,
+        uint amount) private {
+            bool sent = _token.transferFrom(sender, recipient, amount);
+            require(sent, "Token transfer failed");
+    }
 }
