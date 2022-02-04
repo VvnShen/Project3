391713b73d484238778c0d46a40dab31cb40db9d
Vivian Shen <88476898+VvnShen@users.noreply.github.com>
2022-02-03 22:20:09 -0500

    Update USDC_testnet.sol

--- USDC_testnet.sol
+++ USDC_testnet.sol
@@ -1,22 +1,29 @@
-pragma solidity ^0.5.0;
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.4;
+import "github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 
-import "github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20.sol";
-import "github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Detailed.sol";
-
-contract UsdcToken is ERC20, ERC20Detailed {
+contract UsdcToken is ERC20 {
     address payable owner;
 
     modifier onlyOwner {
         require(msg.sender == owner, "You do not have permission to mint these tokens!");
         _;
     }
+    
+    constructor() ERC20("USDCtest", "USDC")  {
+        owner = payable(msg.sender);
+        //default initial supply of the token is 10000USDC (6 decimals)
+        _mint(owner, 10000000000);
 
-    constructor(uint initial_supply) ERC20Detailed("USDC TEST", "USDC", 6) public {
-        owner = msg.sender;
-        _mint(owner, initial_supply);
+    }
+//ERC20 in latest version doesn't support decimals specification, default=18. USDC is 6 decimal units. So I'll overload it.
+    function decimals() public view virtual override returns (uint8) {
+        return 6;
     }
 
     function mint(address recipient, uint amount) public onlyOwner {
         _mint(recipient, amount);
     }
+
+    
 }
