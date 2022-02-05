85620d8ae9a44fef9cd254382d090ba8678edcab
Vivian Shen <88476898+VvnShen@users.noreply.github.com>
2022-02-05 17:33:53 -0500

    hope is the last version

--- DEC_USDCowner.sol
+++ DEC_USDCowner.sol
@@ -3,26 +3,16 @@ pragma solidity ^0.8.4;
 import "github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 import 'USDC_testnet.sol';
 
-
- /** Dex_ETH_owner_trading: initializer owns ETH, and want to swap with USDC. 
-    1) required fileds to be input by initializer (Gather in front end): 
-    * init_symbol(owns Ether/USDC); 
-    * init_amount(how many Eth/USDC want to swap with the other);
-    * initializer(wallet address);
-    * price (rate to convert ETH/USDC)
-    2) State.Locked mechenism to lock the init_amount in contract until order confirmed by the settler, then release it to settler.
-    3) this should work in python for FE: tx_hash = contract.functions.initializer().transact({'from': initializer's adreess, 'gas': 1000000})
-**/
-
+ //Dex_USDC_owner_trading: initializer owns USDC, and want to swap with ETH. 
 contract Dex_USDC_owner_trading {
  
     address payable initializer;
+    address payable settler;
     IUsdcToken public token;
-    bool public transferable = false;
-     mapping(address => uint) balances;
-    
-    //controlling the flowing of funds
-    enum State { Created, Locked, Release, Inactive }
+    bool public transferable = true;
+    mapping(address => uint) balances;
+
+    enum State { Created, Locked, Release, Cancelled }
     State public state;
 
     modifier condition(bool condition_){
@@ -30,58 +20,64 @@ contract Dex_USDC_owner_trading {
         _;
     }
 
+    modifier onlyInitializer (){
+        require (msg.sender==initializer, "please switch to initializer's account for initializing this swap");
+        _;
+    }
+
+    modifier istransferable {
+        require(transferable==true, "token under lock, can Not Trade");
+         _;
+    }
     modifier inState(State state_){
         require (state == state_, "invalid state");
         _;
     }
-    modifier OnlyOwner(){
-        require (msg.sender == initializer, "ETH owner must be the initializer here");
-        _;
-    }
-    modifier istransferable {
-        require(transferable==false, "token under lock, can Not Trade");
-         _;
-    }
 
+    constructor () {
+        initializer = payable(msg.sender);
+    }
 
     event Initialize();
     event SettleSwap();
-    event Transfer();
 
+    
 
-//this contract will be initialized bt whom owns USDC and wants to swap to Ether
-    function initialize(uint init_amount,address payable initializer_,address _token) 
-        external 
-        OnlyOwner 
-        payable {
+//this contract will be initialized by whom owns USDC and wants to swap to Ether
+    function initialize(uint init_amount,address _token) 
+        inState(State.Created) onlyInitializer external  payable {
         emit Initialize();
-        state = State.Locked;
-        initializer=initializer_;
         token = IUsdcToken(_token);
-        require (token.allowance(initializer,address(this)) >= init_amount,"please make sure settler approve the token's delegation");
-        token.isTransferable(true);
+        require (token.allowance(initializer,address(this)) >= init_amount,"please approve the token's delegation to this contract address ") ;
+        token.isTransferable(false);
+        state = State.Locked;
 
     }
 
 
 //amount of Ether to swap will be calculated in FE, here just pass an amount of Ether for settlement (FE: USDC_amount=price*init_amont_in_ETH*1000000)
-
-    function settleSwap (uint init_amount,address payable initializer_, address payable settler,address _token, uint ETH_amount)
-        external
-        inState(State.Locked) OnlyOwner condition(init_amount != 0)  
+    function settleSwap (uint init_amount, address payable settler_,address _token)
+        external 
+        inState(State.Locked) 
+        condition(init_amount != 0)  
+        condition(msg.value !=0)
         payable {
             emit SettleSwap();
-            state = State.Release;
-            initializer=initializer_;
             token = IUsdcToken(_token);
-            require (token.allowance(settler,address(this)) >= ETH_amount,"please make sure settler approve the token's delegation");
-            token.isTransferable(false);
+            settler=settler_;
+            require (msg.sender==settler, "please switch to settler's account for settling this swap");
+            require (settler.balance >= msg.value,"settler, please make sure you have enough ETH amounts to swap");
+            token.isTransferable(true);
+            state = State.Release;
             _safeTransferFrom(token, initializer,settler,init_amount);
-            balances[initializer] += ETH_amount;
-            balances[settler] -= ETH_amount;
-            
-
+            initializer.transfer(msg.value);
         }
+//this function is to withdraw the swap for initizlier before settleswap is completed. partially withdrawal is not supportive
+    function withdrawSwap (address _token) external inState(State.Locked) onlyInitializer {
+        token = IUsdcToken(_token);
+        token.isTransferable(true);
+        state = State.Cancelled;
+    }
 
     function _safeTransferFrom(
         IUsdcToken _token,
@@ -91,4 +87,6 @@ contract Dex_USDC_owner_trading {
             bool sent = _token.transferFrom(sender, recipient, amount);
             require(sent, "Token transfer failed");
     }
+    
+
 }
