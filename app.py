60cc689bfcb2bcf0ed5296d78a0bd1349b1189a6
yauhei721 <88312565+yauhei721@users.noreply.github.com>
2022-02-05 11:54:00 -0600

    updated app.py

--- app.py
+++ app.py
@@ -36,48 +36,57 @@ load_dotenv()
 
 #    return contract
 
-def display_offers(offers):
+def display_offers(offerss):
     output = []     # output strings             
-    for o in offers:
-        str = f"{o['Trade']}; Exchange Rate: {o['Rate']}"
+    for o in offerss:
+        str = f"{o['Trade']}; Exchange Rate: {o['Rate']}; ID: {o['TradeID']}"
         output.append(str)
     
     return output
 
+
 # initialize current offer structure
-offers = [
-    {'Trade': 'Sell', 'Rate': 2500, 'Limit': 10, 'address': '0x788B21A949250f682e260679D0B3aFf2C29b884A'},
-    {'Trade': 'Buy', 'Rate': 2480, 'Limit': 5.5, 'address': '0x250D5C91Af9Df21B473E3ab0c37A6566BA863404'},
-    {'Trade': 'Buy', 'Rate': 'Market', 'Limit': 3.7, 'address': '0x33b80ba090487012aB049380D781b91e39481CA8'}
-]
+if 'offers' not in st.session_state:
+    st.session_state['offers'] = [
+        {'TradeID': 0, 'Trade': 'Sell', 'Rate': 2500, 'Limit': 10, 'address': '0x788B21A949250f682e260679D0B3aFf2C29b884A'},
+        {'TradeID': 1, 'Trade': 'Buy', 'Rate': 2480, 'Limit': 5.5, 'address': '0x250D5C91Af9Df21B473E3ab0c37A6566BA863404'},
+        {'TradeID': 2, 'Trade': 'Buy', 'Rate': 'Market', 'Limit': 3.7, 'address': '0x33b80ba090487012aB049380D781b91e39481CA8'}
+    ]
 
 
 # Welcome to CryptoFlea
 
 st.title("Welcome to Crypto Flea Market!!")
 
+
 # Display list of offers currently in the system
 st.markdown("### Here are offers currently in the market.")
-#st.markdown("```User: Apple; Trade: Sell; Exchange Rate: 2500 USDC```")
+
 
 # Display a list of offers for user to select using a selection box
-offers_str = display_offers(offers)
+offers_str = display_offers(st.session_state['offers'])
 selected_offer = st.selectbox(
-     'Which offer do you want to accept?', options=offers_str)#offers, format_func=display_offers) 
-     #('Sell; Exchange Rate: 2500 USDC', 'Buy; Exchange Rate: 2300 USDC', 'Buy; Exchange Rate: Market Order'))
+    'Which offer do you want to accept?', options=offers_str)
 
 
 transaction_amount = st.number_input("Input transaction amount")
+your_address = st.text_input("Your wallet address")
 if st.button("Transact"):
-    st.markdown(f'{transaction_amount} of ether is transacted')
+    st.markdown(f'You selected {selected_offer}. {transaction_amount} of ether is transacted at {your_address}')
     st.balloons()
+    # remove the offer from the list
+    delim_str = selected_offer.split(';')
+    id = int(delim_str[-1].split(':')[-1])
+
+def add_offer(trade_type, exchange_rate, limit, offer_address):
+    id = st.session_state['offers'][-1] + 1
+    st.session_state['offers'].append({'TradeID': id, 'Trade': trade_type, 'Rate': exchange_rate, 'Limit': limit, 'address': offer_address})
 
 # Here we can invite user to make his/her own offer
 
 st.markdown("### Please make your offer here.")
 trade_type = st.selectbox('Buy or Sell?', ('Buy','Sell'))
-wallet_address = st.text_input("Your wallet address")
+offer_address = st.text_input("Transaction wallet address")
 exchange_rate = st.text_input("Your exchange rate")
-if st.button("Make Offer"):
-    offers.append({'Trade': trade_type, 'Rate': exchange_rate, 'Limit': 0, 'address': wallet_address})
+st.button("Make Offer", on_click=add_offer, args=[trade_type, exchange_rate, 0, offer_address])
 
