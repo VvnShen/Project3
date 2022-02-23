3fbbcc1710a5f3b9f0d0981603f7ae909cb89e08
Vivian Shen <88476898+VvnShen@users.noreply.github.com>
2022-02-22 22:29:10 -0500

    Add files via upload

---
+++ BackEnd/DEX_ETHowner.sol
@@ -0,0 +1,88 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.4;
+import "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import 'BackEnd/USDC_testnet.sol';
+
+
+ //Dex_ETH_owner_trading: initializer owns ETH, and want to swap with USDC. 
+contract Dex_ETH_owner_trading {
+ 
+    address payable initializer;
+    uint init_amount;
+    IUsdcToken public token;
+    
+    //controlling the flowing of funds
+    enum State { Created, Locked, Release, Cancelled }
+    State public state;
+
+    modifier condition(bool condition_){
+        require(condition_);
+        _;
+    }
+
+    modifier inState(State state_){
+        require (state == state_, "invalid state");
+        _;
+    }
+    modifier OnlyOwner(){
+        require (msg.sender == initializer, "ETH owner must be the initializer here");
+        _;
+    }
+
+
+    event Initialize();
+    event SettleSwap();
+
+ 
+    constructor () payable {
+        initializer= payable(msg.sender);
+        init_amount= msg.value;
+
+    }
+
+//this contract will be initialized bt whom owns Ether and wants to swap to UDDC
+    function initialize() 
+        external 
+        //inState(State.Created) 
+        condition(msg.value !=0)
+        payable {
+        emit Initialize();
+        initializer=payable(msg.sender);
+        state = State.Locked;
+
+    }
+
+//once a settler (who would call Dex_USDC_owner_trading) choose to settle this swap, call below function to settle for ETH owner
+//amount of USDC to swap will be calculated in FE, here just pass an amount of USDC for settlement (FE: USDC_amount=price*init_amont_in_ETH*1000000)
+
+
+
+    function settleSwap (address payable settler,address payable _token, uint USDC_amount)
+        external
+        inState(State.Locked) OnlyOwner condition(msg.value != 0)  
+        payable {
+            emit SettleSwap();
+            state = State.Release;
+            token = IUsdcToken(_token);
+            settler.transfer(msg.value);
+            initializer.transfer(msg.value);
+            _safeTransferFrom(token,settler,initializer, USDC_amount);
+        }
+//this function is to withdraw the swap for initizlier before settleswap is completed. partially withdrawal is not supportive
+    function withdrawSwap () external payable inState(State.Locked) OnlyOwner {
+        initializer.transfer(msg.value*2);
+        state = State.Cancelled;
+    }
+
+    function _safeTransferFrom(
+        IUsdcToken _token,
+        address sender,
+        address recipient,
+        uint amount) private  {
+            bool sent = _token.transferFrom(sender, recipient, amount);
+            require(sent, "Token transfer failed");
+    }
+
+
+  
+}
---
+++ BackEnd/USDC_testnet.sol
@@ -0,0 +1,68 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.4;
+import "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+
+//this is to make other contracts to interact with USDC contract
+interface IUsdcToken {
+    function isTransferable(bool _choice) external;
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function approve(address spender, uint256 amount) external  returns (bool);
+}
+
+
+contract UsdcToken is ERC20 {
+    address payable owner;
+    bool public transferable = true;
+    modifier onlyOwner {
+        require(msg.sender == owner, "You do not have permission to mint these tokens!");
+        _;
+    }
+
+    modifier istransferable {
+        require(transferable==true, "token under lock, can Not Trade");
+         _;
+    }
+    
+    constructor() ERC20("USDCtest", "USDC")  {
+        owner = payable(msg.sender);
+        //default initial supply of the token is 10000USDC (6 decimals)
+        _mint(owner, 1000000*1000000);
+
+    }
+//ERC20 in latest version doesn't support decimals specification, default=18. USDC is 6 decimal units. So I'll overload it.
+    function decimals() public view virtual override returns (uint8) {
+        return 6;
+    }
+
+    function mint(address recipient, uint amount) public onlyOwner {
+        _mint(recipient, amount);
+    }
+//overload below functions so to add lock option on USDC smart contracts 
+    function transfer(address recipient, uint256 amount) public istransferable virtual override returns (bool) {
+        _transfer(_msgSender(), recipient, amount);
+        return true;
+    }
+
+    function transferFrom(address sender, address recipient, uint256 amount) public istransferable virtual override returns (bool) {
+        require(allowance(sender,msg.sender) >= amount, "ERC20: Allowance not high enough.");
+        _transfer(sender, recipient, amount);
+        return true;
+    }
+
+    function isTransferable(bool _choice) public {
+        transferable = _choice;
+    }
+    function approve(address spender, uint256 amount) public virtual override returns (bool) {
+        owner = payable(msg.sender);
+        _approve(owner, spender, amount);
+        return true;
+    }
+
+
+    
+}
